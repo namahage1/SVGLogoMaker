bb26a27dba8fd0fb33d043f72a1b385e799a1b9a
Kasumi Akiyama★ <shellyakiyama@gmail.com>
2024-05-05 18:23:49 -0600

    added svg codes

--- index.js
+++ index.js
@@ -1,22 +1,77 @@
 const  inquirer = require("inquirer");
 const colors = require('colors');
+const shape = require('./lib/shapes.js');
+const validateColor = require("validate-color").default;
+const fs = require('fs');
+
 inquirer
 .prompt([
     {
         type:'input',
-        name:'color',
-        message:'what color do you want? (up to three chars or hex)'
+        name:'text',
+        message:'type text (up to three charactors)',
+        validate: (answer) => {
+            if(answer.length <= 3 && answer.length > 0){
+                return true;
+            } else {
+                console.log("\nInvalid text. Please try again!");
+                return false;
+            }
+        }
+    },
+    {
+        type:'input',
+        name:'textColor',
+        message:'what text color do you want? (color keyword or hex)',
+        validate: (answer) => {
+            if(validateColor(answer)) {
+                return true;
+            } else {
+                console.log("\nInvalid color. Please try again!");
+                return false;
+            }
+        }
     },
     {
         type:'list',
         name:'shape',
         message:'what shape do you want?',
         choices:['circle', 'triangle', 'square']
+    },
+    {
+        type:'input',
+        name:'color',
+        message:'what color do you want?(color keyword or hex)',
+        validate: (answer) => {
+            if(validateColor(answer)) {
+                return true;
+            } else {
+                console.log("\nInvalid color. Please try again!");
+                return false;
+            }
+        }
     }
 ]).then((answers)=>{
-    console.log(`${answers.color}`);
+    console.log(`${answers.text}`);
+    console.log(`${answers.textColor}`);
     console.log(`${answers.shape}`);
+    console.log(`${answers.color}`);
+
+
+    const svgCode = `
+    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200">
+        <circle cx="150" cy="100" r="80" fill="${answers.color}"/>
+        <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
+    </svg>
+    `
+
+    fs.writeFile("./output/logo.svg", svgCode, () => {
+        console.log("Logo has been generated!")
+    })
+
 });
+
+
 /*
 GIVEN a command-line application that accepts user input
 WHEN I am prompted for text
